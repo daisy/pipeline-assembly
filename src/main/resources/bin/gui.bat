25dad32beeb5c514f66665624e8821dd8852c7b9
capitancambio <capitan.cambio@gmail.com>
2014-05-06 13:52:44 +0100

    Update gui.bat so it will not export the MAIN variable

--- src/main/resources/bin/gui.bat
+++ src/main/resources/bin/gui.bat
@@ -3,6 +3,8 @@ rem  Gui launcher
 rem 
 
 rem set appropriate main class
+setlocal
 set MAIN=org.daisy.pipeline.swt.launcher.Main
 rem call the pipeline launcher
-%~dp0\pipeline2.bat
+Call %~dp0\pipeline2.bat
+endlocal
