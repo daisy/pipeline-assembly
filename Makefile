daisy/pipeline-assembly

commit 2fb6331410f3e85207dcfdb331e433ae3cca81b4

Split up build script in many small parts (profiles)
and tie everything together with a Makefile.

Note that `make dev-launcher` is implemented usinig the regular "dir"
build and not the "dev-launcher" profile.

Makefile
@@ -1,21 +1,211 @@
-GTAR := gtar
+MVN ?= mvn
+MVN_LOG ?= cat
 DOCKER := docker
 
+.PHONY : default
+ifeq ($(shell uname), Darwin)
+default : dmg
+else
+default : zip-linux
+endif
+
+.PHONY : help
 help :
-	echo "make docker:"            >&2
-	echo "	Build a Docker image"  >&2
-	echo "make check-docker:"      >&2
-	echo "	Test the Docker image" >&2
+	echo "make [default]:"                                                      >&2
+	echo "	Builds the default package for the current platform"                >&2
+	echo "make dmg:"                                                            >&2
+	echo "	Builds a DMG image (Mac OS disk image)"                             >&2
+	echo "make exe:"                                                            >&2
+	echo "	Builds a EXE (Windows installer)"                                   >&2
+	echo "make deb:"                                                            >&2
+	echo "	Builds a DEB (Debian package)"                                      >&2
+	echo "make rpm:"                                                            >&2
+	echo "	Builds a RPM (RedHat package)"                                      >&2
+	echo "make zip-linux:"                                                      >&2
+	echo "	Builds a ZIP for Linux"                                             >&2
+	echo "make all:"                                                            >&2
+	echo "	Builds a DMG, a EXE, a DEB, a RPM and a ZIP for Linux"              >&2
+	echo "make zip-mac:"                                                        >&2
+	echo "	Builds a ZIP for Mac OS"                                            >&2
+	echo "make zip-win:"                                                        >&2
+	echo "	Builds a ZIP for Windows"                                           >&2
+	echo "make zips:"                                                           >&2
+	echo "	Builds a ZIP for each platform"                                     >&2
+	echo "make zip-minimal:"                                                    >&2
+	echo "	Builds a minimal ZIP that will complete itself upon first update"   >&2
+	echo "make docker:"                                                         >&2
+	echo "	Builds a Docker image"                                              >&2
+	echo "make check|check-docker:"                                             >&2
+	echo "	Tests the Docker image"                                             >&2
 
-target/daisy-pipeline :
-	cd target && unzip assembly-*-linux.zip
+assembly/VERSION     := 1.11.2-SNAPSHOT
+assembly/BASEDIR     := .
+MVN_LOCAL_REPOSITORY ?= $(HOME)/.m2/repository
 
-docker : target/daisy-pipeline
-	$(GTAR) -cz \
-	     target/daisy-pipeline \
-	     Dockerfile.without_builder \
-	     --transform='s/Dockerfile.without_builder/Dockerfile/' \
-	| $(DOCKER) build -t daisyorg/pipeline-assembly -
+include deps.mk
 
+.PHONY : all
+all : dmg exe deb rpm zip-linux
+
+.PHONY : zips
+zips : zip-mac zip-linux zip-win
+
+.PHONY : dmg exe deb rpm zip-linux zip-mac zip-win zip-minimal deb-cli rpm-cli
+
+dmg         : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).dmg
+exe         : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).exe
+deb         : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).deb
+rpm         : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).rpm
+zip-linux   : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-linux.zip
+zip-mac     : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-mac.zip
+zip-win     : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-win.zip
+zip-minimal : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-minimal.zip
+deb-cli     : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-cli.deb
+rpm-cli     : $(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-cli.rpm
+
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).exe         : mvn -Punpack-cli-win \
+                                                                                                                       -Punpack-updater-win \
+                                                                                                                       -Punpack-updater-gui-win \
+                                                                                                                       -Passemble-win-dir \
+                                                                                                                       -Ppackage-exe
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).deb         : mvn -Punpack-updater-linux \
+                                                                                                                       -Ppackage-deb
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-linux.zip   : mvn -Punpack-cli-linux \
+                                                                                                                       -Punpack-updater-linux \
+                                                                                                                       -Passemble-linux-zip
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-mac.zip     : mvn -Punpack-cli-mac \
+                                                                                                                       -Punpack-updater-mac \
+                                                                                                                       -Passemble-mac-zip
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-win.zip     : mvn -Punpack-cli-win \
+                                                                                                                       -Punpack-updater-win \
+                                                                                                                       -Punpack-updater-gui-win \
+                                                                                                                       -Passemble-win-zip
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-minimal.zip : mvn -Punpack-updater-mac \
+                                                                                                                       -Punpack-updater-linux \
+                                                                                                                       -Punpack-updater-win \
+                                                                                                                       -Passemble-minimal-zip
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-cli.deb     : mvn -Punpack-cli-linux \
+                                                                                                                       -Ppackage-deb-cli
+ifeq ($(shell uname), Darwin)
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).dmg         : mvn -Punpack-cli-mac \
+                                                                                                                       -Punpack-updater-mac \
+                                                                                                                       -Passemble-mac-dir \
+                                                                                                                       -Ppackage-dmg
+	test -e $@
+else
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).dmg         :
+	@echo "Can not build DMG because not running MacOS" >&2
+	exit 1
+endif
+ifeq ($(shell test -f /etc/redhat-release; echo $$?), 0)
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).rpm         : mvn -Passemble-linux-dir \
+                                                                                                                       -Ppackage-rpm
+	test -e $@
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-cli.rpm     : mvn -Punpack-cli-linux \
+                                                                                                                       -Ppackage-rpm-cli
+	test -e $@
+else
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).rpm \
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-cli.rpm :
+	@echo "Can not build RPM because not running RedHat/CentOS" >&2
+	exit 1
+endif
+
+.PHONY : docker
+docker : target/assembly-$(assembly/VERSION)-linux/daisy-pipeline/bin/pipeline2
+	cp Dockerfile.without_builder target/assembly-$(assembly/VERSION)-linux/Dockerfile
+	cd target/assembly-$(assembly/VERSION)-linux && \
+	$(DOCKER) build -t daisyorg/pipeline-assembly .
+
+.PHONY : dev-launcher
+ifeq ($(shell uname), Darwin)
+dev-launcher : target/assembly-$(assembly/VERSION)-mac/daisy-pipeline/bin/pipeline2
+else
+dev-launcher : target/assembly-$(assembly/VERSION)-linux/daisy-pipeline/bin/pipeline2
+endif
+
+target/assembly-$(assembly/VERSION)-mac/daisy-pipeline/bin/pipeline2   : mvn -Punpack-cli-mac \
+                                                                             -Punpack-updater-mac \
+                                                                             -Passemble-mac-dir
+target/assembly-$(assembly/VERSION)-linux/daisy-pipeline/bin/pipeline2 : mvn -Punpack-cli-linux \
+                                                                             -Punpack-updater-linux \
+                                                                             -Passemble-linux-dir
+
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).exe \
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).deb \
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-linux.zip \
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-mac.zip \
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-win.zip \
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-minimal.zip \
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-cli.deb \
+target/assembly-$(assembly/VERSION)-mac/daisy-pipeline/bin/pipeline2 \
+target/assembly-$(assembly/VERSION)-linux/daisy-pipeline/bin/pipeline2 :
+	test -e $@
+
+.PHONY : check
+check : check-docker
+
+.PHONY : check-docker
 check-docker :
 	bash src/test/resources/test-docker-image.sh
+
+#                          generate-resources      process-resources      prepare-package       package
+#                          ---------------         -----------------      ---------------       -------
+# unpack-cli-mac           unpack-cli-mac
+# unpack-cli-linux         unpack-cli-linux
+# unpack-cli-win           unpack-cli-win
+# unpack-updater-mac       unpack-updater-mac
+# unpack-updater-linux     unpack-updater-linux
+# unpack-updater-win       unpack-updater-win
+# unpack-updater-gui-win   unpack-updater-gui-win
+# assemble-mac-dir                                                        assemble-mac-dir
+# assemble-linux-dir                                                      assemble-linux-dir
+# assemble-win-dir                                                        assemble-win-dir
+# assemble-mac-zip                                                                              assemble-mac-zip
+# assemble-linux-zip                                                                            assemble-linux-zip
+# assemble-win-zip                                                                              assemble-win-zip
+# assemble-minimal-zip                                                                          assemble-minimal-zip
+# package-dmg                                                             install-node          javapackager
+#                                                                         install-appdmg        package-appdmg
+#                                                                         parse-version         attach-dmg
+# package-exe                                                             copy-nsis-resources   package-exe
+# package-deb                                      filter-deb-resources                         package-deb
+# package-deb-cli                                                                               package-deb-cli
+# package-rpm                                                                                   package-rpm
+# package-rpm-cli                                                                               package-rpm-cli
+# dev-launcher             generate-fileinstall-cfg-files                 assemble-dev-dir
+
+PROFILES :=                 \
+	assemble-linux-dir      \
+	assemble-linux-zip      \
+	assemble-mac-dir        \
+	assemble-mac-zip        \
+	assemble-win-dir        \
+	assemble-win-zip        \
+	assemble-minimal-zip    \
+	package-deb             \
+	package-deb-cli         \
+	package-dmg             \
+	package-exe             \
+	package-rpm             \
+	package-rpm-cli         \
+	unpack-cli-linux        \
+	unpack-cli-mac          \
+	unpack-cli-win          \
+	unpack-updater-linux    \
+	unpack-updater-mac      \
+	unpack-updater-win      \
+	unpack-updater-gui-win  \
+	dev-launcher
+
+.PHONY : mvn
+mvn :
+ifndef DUMP_PROFILES
+	$(MVN) clean install $(shell $(MAKE) -qs DUMP_PROFILES=true $(MAKECMDGOALS)) | $(MVN_LOG)
+endif
+
+.PHONY : $(addprefix -P,$(PROFILES))
+ifdef DUMP_PROFILES
+$(addprefix -P,$(PROFILES)) :
+	+echo $@
+endif

.travis.yml
@@ -4,8 +4,7 @@ services:
 before_install:
   - source <(curl -fsSL https://raw.github.com/daisy/maven-parents/travis/before_install)
 script:
-  - mvn clean package -Plinux &&
-    make GTAR=/bin/tar docker &&
+  - make docker &&
     make check-docker
 branches:
   except:

Dockerfile.without_builder
@@ -1,6 +1,6 @@
 FROM openjdk:9-jre
 LABEL maintainer="DAISY Consortium (http://www.daisy.org/)"
-ADD target/daisy-pipeline /opt/daisy-pipeline2
+ADD daisy-pipeline /opt/daisy-pipeline2
 ENV PIPELINE2_WS_LOCALFS=false \
     PIPELINE2_WS_AUTHENTICATION=true \
     PIPELINE2_WS_AUTHENTICATION_KEY=clientid \

deps.mk
@@ -1,0 +1,17 @@
+assembly/SOURCES := $(assembly/BASEDIR)/pom.xml \
+                    $(shell [ -d $(assembly/BASEDIR)/src/main/ ] && find $(assembly/BASEDIR)/src/main/ -type f | sed 's/ /\\ /g')
+
+.SECONDARY : assembly/SOURCES
+assembly/SOURCES : $(assembly/SOURCES)
+
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).dmg         : assembly/SOURCES
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).exe         : assembly/SOURCES
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).deb         : assembly/SOURCES
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION).rpm         : assembly/SOURCES
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-linux.zip   : assembly/SOURCES
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-mac.zip     : assembly/SOURCES
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-win.zip     : assembly/SOURCES
+$(MVN_LOCAL_REPOSITORY)/org/daisy/pipeline/assembly/$(assembly/VERSION)/assembly-$(assembly/VERSION)-minimal.zip : assembly/SOURCES
+
+$(assembly/BASEDIR)/target/assembly-$(assembly/VERSION)-mac/daisy-pipeline/bin/pipeline2   : assembly/SOURCES
+$(assembly/BASEDIR)/target/assembly-$(assembly/VERSION)-linux/daisy-pipeline/bin/pipeline2 : assembly/SOURCES

src/main/nsis/installer.nsi
@@ -180,8 +180,8 @@ section -Main SEC01
 	writeUninstaller "$INSTDIR\uninstall.exe"
 	#setOutPath "$INSTDIR\${PROJECT_ARTIFACT_ID}"
 
-	#Copy the whole daisy-pipeline dir
-	file /r "${PROJECT_BUILD_DIR}\pipeline2-${VERSION}_win\daisy-pipeline"
+	# Copy the whole daisy-pipeline dir
+	file /r "${PROJECT_BUILD_DIR}\assembly-${VERSION}-win\daisy-pipeline"
 	file .\pipeline2-gui.vbs
 	file .\pipeline2-webservice.vbs
 	file .\errorPrompt.vbs

commit f5ff88f3265661dc6a584caede14152e52841a68

Fix pom-to-release.xslt
These bugs were introduced in commits 2fb6331 and db144e1.

Disabled the test for now, it will be fixed in a later commit.

Makefile
@@ -213,7 +213,7 @@ PROFILES :=                 \
 mvn :
 ifndef DUMP_PROFILES
 	set -o pipefail; \
-	$(MVN) clean install $(shell $(MAKE) -qs DUMP_PROFILES=true -- $(MAKECMDGOALS)) | $(MVN_LOG)
+	$(MVN) clean install -DskipTests $(shell $(MAKE) -qs DUMP_PROFILES=true -- $(MAKECMDGOALS)) | $(MVN_LOG)
 endif
 
 .PHONY : $(addprefix -P,$(PROFILES))

src/main/xslt/pom-to-release.xslt
@@ -81,15 +81,15 @@
                                         <xsl:with-param name="deployPath" select="$deployPath"/>
                                 </xsl:apply-templates>
                         </xsl:for-each>
-                        <xsl:apply-templates mode="zip" select="/pom:project/pom:profiles/pom:profile[pom:id='linux']/pom:build/pom:plugins/pom:plugin/pom:executions/pom:execution[./pom:id/text()='unpack-cli-linux']/pom:configuration/pom:artifactItems/pom:artifactItem">
+                        <xsl:apply-templates mode="zip" select="/pom:project/pom:profiles/pom:profile[pom:id='unpack-cli-linux']/pom:build/pom:plugins/pom:plugin/pom:executions/pom:execution[./pom:id/text()='unpack-cli-linux']/pom:configuration/pom:artifactItems/pom:artifactItem">
                                 <xsl:with-param name="deployPath">cli</xsl:with-param>
                                 <xsl:with-param name="classifier">linux_386</xsl:with-param>
                         </xsl:apply-templates>
-                        <xsl:apply-templates mode="zip" select="/pom:project/pom:profiles/pom:profile[pom:id='mac']/pom:build/pom:plugins/pom:plugin/pom:executions/pom:execution[./pom:id/text()='unpack-cli-mac']/pom:configuration/pom:artifactItems/pom:artifactItem">
+                        <xsl:apply-templates mode="zip" select="/pom:project/pom:profiles/pom:profile[pom:id='unpack-cli-mac']/pom:build/pom:plugins/pom:plugin/pom:executions/pom:execution[./pom:id/text()='unpack-cli-mac']/pom:configuration/pom:artifactItems/pom:artifactItem">
                                 <xsl:with-param name="deployPath">cli</xsl:with-param>
                                 <xsl:with-param name="classifier">darwin_386</xsl:with-param>
                         </xsl:apply-templates>
-                        <xsl:apply-templates mode="zip" select="/pom:project/pom:profiles/pom:profile[pom:id='win']/pom:build/pom:plugins/pom:plugin/pom:executions/pom:execution[./pom:id/text()='unpack-cli-win']/pom:configuration/pom:artifactItems/pom:artifactItem">
+                        <xsl:apply-templates mode="zip" select="/pom:project/pom:profiles/pom:profile[pom:id='unpack-cli-win']/pom:build/pom:plugins/pom:plugin/pom:executions/pom:execution[./pom:id/text()='unpack-cli-win']/pom:configuration/pom:artifactItems/pom:artifactItem">
                                 <xsl:with-param name="deployPath">cli</xsl:with-param>
                                 <xsl:with-param name="classifier">windows_386</xsl:with-param>
                         </xsl:apply-templates>
@@ -139,12 +139,6 @@
                                                         if ($classifier) then concat('-',$classifier) else '',
                                                         '.jar')"/>
                 <xsl:variable name="id" select="string-join(($groupId,$artifactId,$version,$classifier),'/')"/>
-                <!--
-                    This is a hack, real solution is to do this in the POM
-                -->
-                <xsl:variable name="deployPath" select="if (starts-with($artifactId,'org.apache.felix.gogo.'))
-                                                        then concat($deployPath,'/gogo')
-                                                        else $deployPath"/>
                 <xsl:variable name="finalPath" select="if (pom:destFileName) then concat($deployPath,'/',string(pom:destFileName)) else
                                                        concat($deployPath,'/',$groupId,'.',$artifactId,'-',$version,
                                                         if ($classifier) then concat('-',$classifier) else '',