f32a283bf7569bf903b51c2d5acdb3f8778115c5
Nicolas Pavie <nicolas.pavie@braillenet.org>
2021-10-27 12:21:29 +0200

    Modify SimpleAPI so that it can be used as bridge interface for JNI

--- src/main/java/SimpleAPI.java
+++ src/main/java/SimpleAPI.java
@@ -1,8 +1,15 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.daisy.common.messaging.Message;
+import org.daisy.common.messaging.Message.Level;
+import org.daisy.common.messaging.MessageAccessor;
 import org.daisy.common.spi.CreateOnStart;
 import org.daisy.common.spi.ServiceLoader;
 import org.daisy.common.transform.LazySaxResultProvider;
@@ -29,11 +36,12 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 
 /**
- * A simplified Java API consisting of a single {@link #runJob()}
- * method that accepts a script name and a list of options and returns
- * a {@link Job}. It is used to build a simple Java CLI (see the
- * {@link #main()} method). The simplified API also makes it easier to
- * bridge with other programming languages using JNI.
+ * A simplified Java API consisting of a {@link #startJob()} method
+ * that accepts a script name and a list of options and returns a
+ * {@link Job}, a {@link #deleteJob()} method, and a {@link
+ * #getNewMessages()} method. It is used to build a simple Java CLI
+ * (see the {@link #main()} method). The simplified API also makes it
+ * easier to bridge with other programming languages using JNI.
  */
 @Component(
 	name = "SimpleAPI",
@@ -78,10 +86,12 @@ public class SimpleAPI {
 		this.jobManagerFactory = jobManagerFactory;
 	}
 
-	private void runJob(String scriptName, Map<String,String> options) throws InterruptedException, IOException {
+	private JobManager jobManager = null;
+
+	private Job _startJob(String scriptName, Map<String,String> options) throws IOException {
 		XProcScriptService scriptService = scriptRegistry.getScript(scriptName);
 		if (scriptService == null)
-			throw new IllegalStateException();
+			throw new IllegalArgumentException(scriptName + " script not found");
 		XProcScript script = scriptService.load();
 		File cwd = new File(System.getProperty("org.daisy.pipeline.cli.cwd", "."));
 		XProcInput.Builder inputs = new XProcInput.Builder(); {
@@ -91,13 +101,11 @@ public class SimpleAPI {
 					if (!file.isAbsolute())
 						file = new File(cwd, file.getPath()).getCanonicalFile();
 					if (!file.exists()) {
-						System.err.println("File does not exist: " + file);
-						System.exit(1);
+						throw new FileNotFoundException(file.getPath());
 					}
-					inputs.withInput(port.getName(), new LazySaxSourceProvider(file.getPath()));
+					inputs.withInput(port.getName(), new LazySaxSourceProvider(file.toURI().toURL().toString()));
 				} else if (script.getPortMetadata(port.getName()).isRequired()) {
-					System.err.println("Required option --" + port.getName() + " missing");
-					System.exit(1);
+					throw new IllegalArgumentException("Required option " + port.getName() + " missing");
 				}
 			}
 			for (XProcOptionInfo option : script.getXProcPipelineInfo().getOptions()) {
@@ -113,41 +121,35 @@ public class SimpleAPI {
 							file = new File(cwd, file.getPath()).getCanonicalFile();
 						if (metadata.getOutput() == XProcOptionMetadata.Output.RESULT) {
 							if (file.exists()) {
-								System.err.println("File exists: " + file);
-								System.exit(1);
+								throw new IllegalArgumentException("File exists: " + file);
 							}
 						} else {
 							if (!file.exists()) {
-								System.err.println("File does not exist: " + file);
-								System.exit(1);
+								throw new FileNotFoundException(file.getPath());
 							}
 						}
-						value = file.getPath();
+						value = file.toURI().toURL().toString();
 					} else if ("anyDirURI".equals(type)) {
 						File dir = new File(value);
 						if (!dir.isAbsolute())
 							dir = new File(cwd, dir.getPath()).getCanonicalFile();
 						if (dir.exists() && !dir.isDirectory()) {
-							System.err.println("Not a directory: " + dir);
-							System.exit(1);
+							throw new IllegalArgumentException("Not a directory: " + dir);
 						}
 						if (metadata.getOutput() == XProcOptionMetadata.Output.RESULT) {
 							if (dir.exists() && dir.list().length > 0) {
-								System.err.println("Directory is not empty: " + dir);
-								System.exit(1);
+								throw new IllegalArgumentException("Directory is not empty: " + dir);
 							}
 						} else {
 							if (!dir.exists()) {
-								System.err.println("Directory does not exist: " + dir);
-								System.exit(1);
+								throw new FileNotFoundException(dir.getPath());
 							}
 						}
-						value = dir.getPath();
+						value = dir.toURI().toURL().toString();
 					}
 					inputs.withOption(option.getName(), value);
 				} else if (option.isRequired() && metadata.getOutput() != XProcOptionMetadata.Output.RESULT) {
-					System.err.println("Required option --" + option.getName() + " missing");
-					System.exit(1);
+					throw new IllegalArgumentException("Required option " + option.getName() + " missing");
 				}
 			}
 		}
@@ -161,45 +163,88 @@ public class SimpleAPI {
 					if (file.isDirectory() || filePath.endsWith("/") || port.isSequence()) {
 						if (file.isDirectory()) {
 							if (file.list().length > 0) {
-								System.err.println("Directory is not empty: " + file);
-								System.exit(1);
+								throw new IllegalArgumentException("Directory is not empty: " + file);
 							}
 						} else if (file.exists()) {
-							System.err.println("Not a directory: " + file);
-							System.exit(1);
+							throw new IllegalArgumentException("Not a directory: " + file);
 						}
-						filePath = file.getPath() + "/";
+						filePath = file.toURI().toURL().toString() + "/";
 					} else {
 						if (file.exists()) {
-							System.err.println("File exists: " + file);
-							System.exit(1);
+							throw new IllegalArgumentException("File exists: " + file);
 						}
-						filePath = file.getPath();
+						filePath = file.toURI().toURL().toString();
 					}
 					outputs.withOutput(port.getName(), new LazySaxResultProvider(filePath));
 				}
 			}
 		}
 		for (String unknown : options.keySet()) {
-			System.err.println("Unknown option: " + unknown);
-			System.exit(1);
+			throw new IllegalArgumentException("Unknown option: " + unknown);
 		}
 		BoundXProcScript boundScript = BoundXProcScript.from(script, inputs.build(), outputs.build());
-		Client client = webserviceStorage.getClientStorage().defaultClient();
-		JobManager jobManager = jobManagerFactory.createFor(client);
+		if (jobManager == null) {
+			Client client = webserviceStorage.getClientStorage().defaultClient();
+			jobManager = jobManagerFactory.createFor(client);
+		}
 		Job job = jobManager.newJob(boundScript).isMapping(true).build().get();
-		while (true) {
-			switch (job.getStatus()) {
-			case SUCCESS:
-			case FAIL:
-			case ERROR:
-				return;
-			case IDLE:
-			case RUNNING:
-			default:
-				Thread.sleep(1000);
+		MessageAccessor accessor = job.getContext().getMonitor().getMessageAccessor();
+		accessor.listen(
+			num -> {
+				consumeMessage(accessor, num);
+			}
+		);
+		return job;
+	}
+
+	public static Job startJob(String scriptName, Map<String,String> options) throws IOException {
+		return getInstance()._startJob(scriptName, options);
+	}
+
+	private static void deleteJob(Job job) {
+		if (INSTANCE == null || INSTANCE.jobManager == null)
+			throw new IllegalStateException();
+		INSTANCE.jobManager.deleteJob(job.getId());
+	}
+
+	/**
+	 * Singleton thread safe instance of SimpleAPI.
+	 */
+	private static SimpleAPI INSTANCE;
+	private static SimpleAPI getInstance() {
+		if (INSTANCE == null) {
+			for (CreateOnStart o : ServiceLoader.load(CreateOnStart.class))
+				if (INSTANCE == null && o instanceof SimpleAPI)
+					INSTANCE = (SimpleAPI)o;
+			if (INSTANCE == null)
+				throw new IllegalStateException();
+		}
+		return INSTANCE;
+	}
+
+	public static List<Message> messagesQueue = new ArrayList<>();
+	public static int lastMessage = -1;
+	private static synchronized void consumeMessage(MessageAccessor accessor, int seqNum) {
+		for (Message m :
+				accessor.createFilter()
+				        .greaterThan(lastMessage)
+				        .filterLevels(Collections.singleton(Level.INFO))
+				        .getMessages()) {
+			if (m.getSequence() > lastMessage) {
+				messagesQueue.add(m);
 			}
 		}
+		lastMessage = seqNum;
+	}
+
+	/**
+	 * Get the list of new top-level messages (messages that have not
+	 * been returned yet by a previous call to {@link #getNewMessages()}).
+	 */
+	public static synchronized List<Message> getNewMessages() {
+		List<Message> result = List.copyOf(messagesQueue);
+		messagesQueue.clear();
+		return result;
 	}
 
 	/**
@@ -224,13 +269,30 @@ public class SimpleAPI {
 			}
 			options.put(option, args[i + 1]);
 		}
-		SimpleAPI api = null;
-		for (CreateOnStart o : ServiceLoader.load(CreateOnStart.class))
-			if (api == null && o instanceof SimpleAPI)
-				api = (SimpleAPI)o;
-		if (api == null)
-			throw new IllegalStateException();
-		api.runJob(script, options);
-		System.exit(0);
+		Job job = null;
+		try {
+			job = SimpleAPI.startJob(script, options);
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+			System.exit(1);
+		} catch (FileNotFoundException e) {
+			System.err.println("File does not exist: " + e.getMessage());
+			System.exit(1);
+		}
+		while (true) {
+			for (Message m : SimpleAPI.getNewMessages()) {
+				System.err.println(m.getText());
+			}
+			switch (job.getStatus()) {
+			case SUCCESS:
+			case FAIL:
+			case ERROR:
+				System.exit(0);
+			case IDLE:
+			case RUNNING:
+			default:
+				Thread.sleep(1000);
+			}
+		}
 	}
 }
